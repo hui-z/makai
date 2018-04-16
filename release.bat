a375ac110cd47fdb58739f98dae9fa58a536e84d
hulucc <zhujunru@kezaihui.com>
2018-04-16 20:11:58 +0800

    fix: release to net461

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 @echo off
 git pull
 nuget restore
-msbuild makai.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.5.2
+msbuild makai.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
